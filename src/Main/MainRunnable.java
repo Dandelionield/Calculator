0b0f888712085522b97a1b4ab8f7d717f8f6a50e
Dandelionield <felipemaysalguedo@gmail.com>
2024-06-01 20:13:11 -0500

    Uploading files

---
+++ src/Main/MainRunnable.java
@@ -0,0 +1,25 @@
+package Main;
+
+/*
+ *
+ * @author Dandelion
+ * 
+ */
+
+import Taylor.Arithmetic.Parser;
+
+public class MainRunnable{
+
+    public static void main(String[] abc){
+		
+		System.out.print("\n\n");
+		
+		Parser p = new Parser("e");
+    
+        System.out.print(p);
+		
+		System.out.print("\n\n");
+		
+    }
+    
+}
